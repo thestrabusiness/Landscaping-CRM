0640964fe5e74c9799b15fec2d03ddc935177026
thestrabusiness <moffa.an@gmail.com>
2016-01-15 22:46:49 -0500

    building out autocomplete search field

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,14 @@ class Invoice < ActiveRecord::Base
   belongs_to :client
   has_many :services, dependent: :destroy
   
+  def client_last_name
+    client.try(:last_name)
+  end
+  
+  def client_last_name=(last_name)
+    self.client = Client.find_by_name(last_name) if last_name.present?
+  end
+  
   def set_sent
   end
   
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,2 @@
-listen "192.168.1.122:3000"
+listen "192.168.1.20:3000"
 worker_processes 4
