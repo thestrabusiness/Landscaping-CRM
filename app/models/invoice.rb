9a7ea98a25f35f9dbc3db7b3c61a2b000d2323ee
thestrabusiness <moffa.an@gmail.com>
2016-07-06 21:04:07 -0400

    workaround for invoice amount/client balance calculations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -9,13 +9,29 @@ class Invoice < ActiveRecord::Base
   def client_name=(name)
 #    self.client = Client.find_by(:last_name => lastname) if lastname.present?
   end
+  
+  def check_balance_forward
+    if client.balance == 0
+      0
+    elsif (client.balance - total < 0)
+    	1
+    else
+      2
+    end
+  end
 
   def balance_forward
     if client.balance == 0
       0
+    elsif (client.balance - total < 0)
+    	
     else
       (client.balance - total)
     end
+  end
+		
+	def amount_due
+    total + balance_forward
   end
 
   def invoice_summary
