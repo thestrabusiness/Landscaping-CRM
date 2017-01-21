69a1a4ae812d0676d0d343a9c15d8f662dbd218d
Anthony M <anthony@followapp.me>
2017-01-21 15:05:58 -0500

    update tests and config files

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module MoffaLandscaping
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.web_console.development_only = false
     #config.middleware.use "PDFKit::Middleware"
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,11 @@
 FactoryGirl.define do
 
   factory :user do
-    email                 "johnson@gmail.com"
-    password              'password1'
-    password_confirmation 'password1'
-    confirmed_at          Date.today
+
+    # email                 "johnson@gmail.com"
+    # password              'password1'
+    # password_confirmation 'password1'
+    # confirmed_at          Date.today
   end
 
 end
