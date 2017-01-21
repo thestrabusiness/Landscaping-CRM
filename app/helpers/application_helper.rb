9d348b5de4a9f71f001f19a2631d4fde8676629a
Anthony M <thestrabusiness@users.noreply.github.com>
2017-01-21 15:09:16 -0500

    Update to rails 5
    * update to rails 5
    
    * update tests and config files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, params.merge(:sort => column, :direction => direction, :page => nil)
+    link_to title, params.permit(:sort, :direction, :page).merge(:sort => column, :direction => direction, :page => nil)
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,11 @@
 FactoryGirl.define do
-  
+
   factory :user do
-    email               "johnson@gmail.com"
-    password            Devise.bcrypt(User, 'password')
-    confirmed_at        Date.today
+
+    # email                 "johnson@gmail.com"
+    # password              'password1'
+    # password_confirmation 'password1'
+    # confirmed_at          Date.today
   end
-    
+
 end
